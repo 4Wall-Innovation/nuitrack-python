83c89678edd6c347da188c495134391b6f311fc4
JakeLewisMedler <jmedler@4wall.com>
2023-08-25 17:05:35 +0100

    init

---
+++ run.py
@@ -0,0 +1,61 @@
+from PyNuitrack import py_nuitrack
+import keyboard
+
+nuitrack = py_nuitrack.Nuitrack()
+nuitrack.init()
+
+devices = nuitrack.get_device_list()
+for i, dev in enumerate(devices):
+	print(dev.get_name(), dev.get_serial_number())
+	if i == 0:
+		#dev.activate("ACTIVATION_KEY") #you can activate device using python api
+		print(dev.get_activation())
+		nuitrack.set_device(dev)
+
+print(nuitrack.get_version())
+print(nuitrack.get_license())
+
+nuitrack.create_modules()
+nuitrack.run()
+
+def sortFunction(e):
+	return e.torso.real[2]
+
+def checkTorsoPosition(torso):
+	threshold = 200
+	if(torso["x"]>threshold):
+		return 1
+	if(torso["x"]<-threshold):
+		return -1
+	return 0
+
+def checkHandPositions(left,right):
+	# print(left["y"])
+	threshold = 900
+	if(left["y"]>threshold or right["y"]>threshold):
+		return 1
+	else:
+		return 0
+	
+while 1:
+	nuitrack.update()
+	data = nuitrack.get_skeleton()
+	data.skeletons.sort(key=sortFunction)
+	if(len(data.skeletons)>0):
+		skeleton = data.skeletons[0]
+		torso = {"x":skeleton.torso.real[0],"y":skeleton.torso.real[1],"z":skeleton.torso.real[2]}
+		right_hand = {"x":skeleton.right_hand.real[0],"y":skeleton.right_hand.real[1],"z":skeleton.right_hand.real[2]}
+		left_hand = {"x":skeleton.left_hand.real[0],"y":skeleton.left_hand.real[1],"z":skeleton.left_hand.real[2]}
+
+		if(checkHandPositions(left_hand,right_hand)==1):
+			print("FIRE")
+			keyboard.press_and_release('space')
+		if(checkTorsoPosition(torso)==1):
+			print("LEFT")
+			keyboard.press_and_release('left')
+		if(checkTorsoPosition(torso)==-1):
+			print("RIGHT")
+			keyboard.press_and_release('right')
+
+
+nuitrack.release()
